b42e069c6203a3e379ce5a50b04216eac03e90d5
Martina Pincione <martinapin221@gmail.com>
2022-12-30 22:07:50 -0500

    first commit

---
+++ src/client/index.ts
@@ -0,0 +1,16 @@
+import { Client, GatewayIntentBits } from 'discord.js'
+import keys from '../keys'
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+  ],
+})
+
+
+client.login(keys.clientToken)
+  .catch((err) => {
+    console.error('[Login Error]', err)
+    process.exit(1)
+  })
